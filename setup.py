da8849ec7bafb9ec149b0d3164fa9782ca6400d2
Patrick Golden <ptgolden0@gmail.com>
2013-07-24 09:54:03 -0400

    Bump to version .11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-licensing',
-    version='0.1',
+    version='0.11',
     packages=['licensing'],
     include_package_data=True,
     license='Public Domain',
