f2d60e743bd5a1d0e036409f6c5545baae43f472
Patrick Golden <ptgolden0@gmail.com>
2014-06-25 10:48:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description='A Django model and data for adding licensing info to data.',
     long_description=README,
     url='http://github.com/editorsnotes/django-licensing',
-    download_url='http://github.com/editorsnotes/django-licensing/tarball/0.2.1',
+    download_url='http://github.com/editorsnotes/django-licensing/tarball/0.2.2',
     author='Ryan Shaw',
     author_email='ryanshaw@unc.edu',
     keywords = ['django', 'licenses', 'licences'],
