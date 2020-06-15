97373d398423659ac6e9d78c9590534201bdfb6e
Ryan Shaw <ryanshaw@unc.edu>
2020-06-15 14:27:49 -0400

    get tests running again

--- licensing/test_settings.py
+++ licensing/test_settings.py
@@ -1,6 +1,9 @@
-DEBUG=True
+import os
 
-DATABASES={
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
     }
@@ -8,11 +11,40 @@ DATABASES={
 
 SECRET_KEY = 'secret'
 
-INSTALLED_APPS=(
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.admin',
     'licensing',
     'tests',
 )
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [
+        os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates')),
+    ],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.contrib.auth.context_processors.auth',
+            'django.template.context_processors.debug',
+            'django.template.context_processors.media',
+            'django.template.context_processors.static',
+            'django.template.context_processors.tz',
+            'django.contrib.messages.context_processors.messages',
+            'django.template.context_processors.request',
+        ]
+    }
+}]
--- tests/tests.py
+++ tests/tests.py
@@ -9,7 +9,7 @@ from django.test import TestCase
 from licensing.models import License
 from .models import Whatever
 from django.template import Template, Context, TemplateDoesNotExist
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 
 class SubclassTests(TestCase):
@@ -17,33 +17,45 @@ class SubclassTests(TestCase):
         whatever = Whatever(license_id=1)
         whatever.save()
 
+
 class LicenseTests(TestCase):
     def test_save(self):
         n_before = License.objects.all().count()
 
-        license = License(name='foobar', symbols=u'\xf0\x9f\x92\xa9', url='http://example.com')
-        license.full_clean() # https://code.djangoproject.com/ticket/13100
+        license = License(
+            name='foobar',
+            symbols=u'\xf0\x9f\x92\xa9',
+            url='http://example.com'
+        )
+        license.full_clean()  # https://code.djangoproject.com/ticket/13100
         license.save()
 
         n_after = License.objects.all().count()
 
         self.assertEqual(n_after, n_before + 1)
 
+
 class TemplateTagsTests(TestCase):
     def _get_random_template(self):
         return License.objects.order_by('?').first()
 
     def test_default_template(self):
-        license = License.objects.get(pk=1) # no random, we need a known symbol
+        license = License.objects.get(pk=1)  # no random, need a known symbol
         html = '''{% load licensing_tags %}{% show_license license %}'''
         template = Template(html)
-        render = template.render(Context({'license': license}))
+        rendered = template.render(Context({'license': license}))
 
-        self.assertEqual(render, u'<i class="openwebicon">©</i>', 'Failed to render known license')
+        self.assertEqual(
+            rendered,
+            u'<i class="openwebicon">©</i>',
+            'Failed to render known license')
 
     def test_not_existing_template(self):
         license = self._get_random_template()
-        html = '''{% load licensing_tags %}{% show_license license template="idontexist.html" %}'''
+        html = (
+            '{% load licensing_tags %}' +
+            '{% show_license license template="idontexist.html" %}'
+        )
         template = Template(html)
 
         with self.assertRaisesMessage(TemplateDoesNotExist, ''):
@@ -51,22 +63,25 @@ class TemplateTagsTests(TestCase):
 
     def test_custom_template(self):
         license = self._get_random_template()
-        html = '''{% load licensing_tags %}{% show_license license template="tests/dummy.html" %}'''
+        html = (
+            '{% load licensing_tags %}' +
+            '{% show_license license template="tests/dummy.html" %}'
+        )
         template = Template(html)
 
-        render = template.render(Context({'license': license}))
+        rendered = template.render(Context({'license': license}))
 
-        self.assertEqual(render, 'FOObar')
+        self.assertEqual(rendered, 'FOObar')
 
     def test_setup_css(self):
 
         with self.assertTemplateUsed('licensing/setup.html'):
-            render = render_to_response('tests/test_setup.html')
+            render(None, 'tests/test_setup.html')
 
     def test_setup_custom_class_css(self):
-        render = None
+        rendered = None
 
         with self.assertTemplateUsed('licensing/setup.html'):
-            render = render_to_response('tests/test_setup_custom.html')
+            rendered = render(None, 'tests/test_setup_custom.html')
 
-        self.assertContains(render, 'foobar')
+        self.assertContains(rendered, 'foobar')
